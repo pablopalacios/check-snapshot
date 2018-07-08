de66e82aa5de4f5af613cb1acaffe06d8bd51c01
Palácios <ppalacios992@gmail.com>
2018-07-08 15:52:55 -0300

    package.json: removed babel-preset-react dependency

--- __tests__/check-snapshot.js
+++ __tests__/check-snapshot.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { iWasCalled } from "../snapshot-serializer";
-
 import checkSnapshot from "../";
 
-const FixtureComponent = () => <p>I'm a fixture component</p>;
+const FixtureComponent = React.createElement(
+  "p",
+  null,
+  "I'm a fixture component"
+);
 
 describe("check-snapshot", () => {
   it("works...", () => {
-    expect(() => checkSnapshot(<FixtureComponent />)).toThrow(iWasCalled());
+    expect(() => checkSnapshot(FixtureComponent)).toThrow(iWasCalled());
   });
 });
