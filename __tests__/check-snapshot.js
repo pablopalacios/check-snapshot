c35ab00da0aa60df016ecb3b45e7ffd96daecd09
palacios <ppalacios992@gmail.com>
2018-12-25 16:54:37 +0100

    index: enabled renderer.create to receive custom options

--- __tests__/check-snapshot.js
+++ __tests__/check-snapshot.js
@@ -1,4 +1,5 @@
 const React = require("react");
+const renderer = require("react-test-renderer");
 const iWasCalled = require("../snapshot-serializer").iWasCalled;
 const checkSnapshot = require("../");
 
@@ -11,5 +12,17 @@ const FixtureComponent = React.createElement(
 describe("check-snapshot", () => {
   it("works...", () => {
     expect(() => checkSnapshot(FixtureComponent)).toThrow(iWasCalled());
+  });
+
+  it("can pass options to renderer", () => {
+    jest.mock("react-test-renderer");
+
+    const options = { some: "option" };
+    renderer.create = jest.fn(() => ({ toJSON: jest.fn() }));
+
+    expect(() => checkSnapshot(FixtureComponent, options)).toThrow(
+      iWasCalled()
+    );
+    expect(renderer.create).toHaveBeenCalledWith(FixtureComponent, options);
   });
 });
